9d815dd4f09e80006744355c492160ca3fb5587a
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-03 16:37:19 +0200

    [1672] [Time Beats: 100.00%] [Memory Beats: 84.07%] - LeetPush

--- 1672-Richest-Customer-Wealth.cpp
+++ 1672-Richest-Customer-Wealth.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        vector<int> rich;
+
+        int res=0;
+        int sum;
         for(int i =0;i<accounts.size();i++){
-            int sum=0;
-            for(int j=0; j<accounts[0].size();j++){
-                sum+=accounts[i][j];
-            }
-            rich.push_back(sum);
+            sum = accumulate(accounts[i].begin(),accounts[i].end(),0);
+            res = max(res,sum);
         }
-        sort(rich.rbegin(),rich.rend());
-        return(rich[0]);
+        
+        return res;
         
 
     }
