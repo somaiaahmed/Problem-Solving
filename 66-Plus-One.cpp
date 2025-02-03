3e9a7405be33403f21e0755e0915aff609725208
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-03 16:17:30 +0200

    [66] [Time Beats: 100.00%] [Memory Beats: 87.81%] - LeetPush

--- 66-Plus-One.cpp
+++ 66-Plus-One.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
+        bool flag=false;
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i] += 1;
-                return digits;  
+                flag=true; 
+                break; 
             }
             digits[i] = 0;  
         }
+        if(flag)
+            return digits;
         digits.insert(digits.begin(), 1);
         return digits;
     }
