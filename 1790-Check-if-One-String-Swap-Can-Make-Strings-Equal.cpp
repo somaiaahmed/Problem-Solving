e50329c18f64d172525b563188fc56addb9f285c
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-05 21:01:25 +0200

    [1790] [Time Beats: 100.00%] [Memory Beats: 9.35%] - LeetPush

---
+++ 1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int swap=0,sum1=0,sum2=0;
+        bool flag=1;
+        
+       map<char,int> map1;
+       map<char,int>map2;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]!=s2[i])
+            swap++;
+        }
+        for(int i=0;i<s1.size();i++){
+            map1[s1[i]]++;
+        }
+        for(int i=0;i<s1.size();i++){
+            map1[s2[i]]--;
+        }
+        for(auto c:map1){
+            if(c.second!=0)
+            flag=0;
+
+        }
+        return (swap==2||swap==0) && flag;
+        
+    }
+};
