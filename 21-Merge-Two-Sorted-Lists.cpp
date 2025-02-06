14e53a00158a835340017db708a8cfb35be21eac
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-06 14:00:32 +0200

    [21] [Time Beats: 100.00%] [Memory Beats: 28.80%] - LeetPush

--- 21-Merge-Two-Sorted-Lists.cpp
+++ 21-Merge-Two-Sorted-Lists.cpp
@@ -30,33 +30,33 @@ public:
                 list2=list2->next;
             }
             
-        ListNode* ans = head;
+        ListNode* curr = head;
 
         while(list1 && list2){
             if(list1->val < list2->val){
-                head->next=list1;
+                curr->next=list1;
                 list1=list1->next;
             }
             else{
-                head->next=list2;
+                curr->next=list2;
                 list2=list2->next;
             }
-            head = head->next;
+            curr = curr->next;
         }   
 
         while(list1){
-            head->next=list1;
+            curr->next=list1;
             list1=list1->next;
-            head = head->next;
+            curr = curr->next;
         }
 
         while(list2){
-            head->next=list2;
+            curr->next=list2;
             list2=list2->next;
-            head = head->next;
+            curr = curr->next;
 
         }
 
-        return ans;
+        return head;
     }
 };
