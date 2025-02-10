2aedff20c98a1a2aaf7597372aad317cfb37d149
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-11 00:19:46 +0200

    [1455] [Time Beats: 100.00%] [Memory Beats: 97.22%] - LeetPush

--- 1455-Check-If-a-Word-Occurs-As-a-Prefix-of-Any-Word-in-a-Sentence.cpp
+++ 1455-Check-If-a-Word-Occurs-As-a-Prefix-of-Any-Word-in-a-Sentence.cpp
@@ -3,17 +3,20 @@ public:
     int isPrefixOfWord(string sentence, string searchWord) {
 
         int index=1;
-        istringstream iss(sentence);
-        string word;
-        while(iss>> word){
-           
-            if(word.find(searchWord)==0)
-            return index;
-
-            index ++;
+        for(int i = 0; i < sentence.size(); i++){
+            int j = 0;
+            while(j < searchWord.size() && searchWord[j] == sentence[i]){
+                i++;
+                j++;
+            }
+            if(j == searchWord.size())
+                return index;
+            while(i < sentence.size() && sentence[i] != ' '){
+                i++;
+            }
+            index++;
         }
         return -1;
-
         
     }
 };
