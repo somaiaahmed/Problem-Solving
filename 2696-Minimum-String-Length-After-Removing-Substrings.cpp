8546146588e58584e76e357d14ff62b1a42b7b4c
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-10 23:12:37 +0200

    [2696] [Time Beats: 12.72%] [Memory Beats: 45.95%] - LeetPush

--- 2696-Minimum-String-Length-After-Removing-Substrings.cpp
+++ 2696-Minimum-String-Length-After-Removing-Substrings.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int minLength(string s) {
-        while(s.find("AB") != -1 || s.find("CD") != -1){
-            if(s.find("AB") != -1)
-                s.erase(s.find("AB"), 2);
-            if(s.find("CD") != -1)
-                s.erase(s.find("CD"), 2);
+        stack<char> stack;
+
+        for(int i = 0; i< s.length(); i++){
+            char currentChar = s[i];
+
+            if(stack.empty()){
+                stack.push(currentChar);
+                continue;
+            }
+            if(currentChar == 'B' && stack.top() == 'A'){
+                stack.pop();
+            }
+            else if(currentChar == 'D' && stack.top() == 'C'){
+                stack.pop();
+            }
+            else{
+                stack.push(currentChar);
+            }
+
         }
-        return s.size();
+        return stack.size();
     }
 };
