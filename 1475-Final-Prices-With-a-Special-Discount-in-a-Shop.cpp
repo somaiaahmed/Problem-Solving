4e71116006f490ea517a9bf4875f66a2de1d8020
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-12 00:55:33 +0200

    [1475] [Time Beats: 100.00%] [Memory Beats: 74.35%] - LeetPush

---
+++ 1475-Final-Prices-With-a-Special-Discount-in-a-Shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        for(int i = 0;i<prices.size()-1;i++){
+            // int j = i+1;
+             for(int j = i+1;j<prices.size();j++){
+                if(prices[i] >= prices[j]){
+                    prices[i]-=prices[j];
+                    break;
+                    
+                    
+                }
+                
+            }
+        }
+        return prices;
+    }
+};
