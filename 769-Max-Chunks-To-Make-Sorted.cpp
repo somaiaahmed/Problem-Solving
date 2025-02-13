8da6a767f6d1114bc23f49e799d7431b032a4e1c
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-13 23:56:17 +0200

    [769] [Time Beats: 100.00%] [Memory Beats: 49.80%] - LeetPush

--- 769-Max-Chunks-To-Make-Sorted.cpp
+++ 769-Max-Chunks-To-Make-Sorted.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> pre = arr, suf = arr;
-
-        for(int i = 1; i < pre.size(); i++){
-            pre[i] = max(pre[i], pre[i - 1]);
+        int sum = 0, sumIdx = 0, cnt = 0;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr[i];
+            sumIdx+=i;
+            cnt += sum == sumIdx;
         }
-        for(int i = suf.size() - 2; i >= 0; i--){
-            suf[i] = min(suf[i], suf[i + 1]); 
-        }
-        int cnt = 0;
-        for(int i = 0; i < n - 1; i++){
-            if(pre[i] < suf[i + 1])
-                cnt++;
-        }
-        return cnt+1;
+        return cnt;
     }
 };
