7057115bcc4f32175b969111d09f91c8de07e001
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-14 00:25:07 +0200

    [2779] [Time Beats: 44.42%] [Memory Beats: 63.18%] - LeetPush

--- 2779-Maximum-Beauty-of-an-Array-After-Applying-Operation.cpp
+++ 2779-Maximum-Beauty-of-an-Array-After-Applying-Operation.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
+    bool ok(vector<int>& nums, int k, int md){
+        int n = nums.size();
+        for(int i = 0; i <= n - md; i++)
+            if(nums[i] + k >= nums[i + md - 1] - k)
+                return true;
+        return false;
+    }
     int maximumBeauty(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int right = 0;  // Pointer for the end of the valid range
-        int maxBeauty = 0;
-
-        // Iterate through the array with the left pointer
-        for (int left = 0; left < nums.size(); left++) {
-            // Expand the right pointer while the range condition is met
-            while (right < nums.size() && nums[right] - nums[left] <= 2 * k) {
-                right++;
+        int st = 1, ed = nums.size(), cur = -1, md;
+        while(st <= ed){
+            md = (st + ed) >> 1;
+            if(ok(nums, k, md)){
+                cur = md;
+                st = md + 1;
             }
-            // Update the maximum beauty based on the current range
-            // We do not add 1 here as right is already pointing to one position
-            // beyond the valid range.
-            maxBeauty = max(maxBeauty, right - left);
+            else
+                ed = md - 1;
+
         }
-        return maxBeauty;
+        return cur;
+
     }
 };
