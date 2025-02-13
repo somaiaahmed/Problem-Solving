8eb09d8a84bc482704aca5e9fe6786e8376fdff4
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-13 22:50:07 +0200

    [2134] [Time Beats: 100.00%] [Memory Beats: 25.60%] - LeetPush

--- 2134-Minimum-Swaps-to-Group-All-1's-Together-II.cpp
+++ 2134-Minimum-Swaps-to-Group-All-1's-Together-II.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
-        int len = accumulate(nums.begin(), nums.end(), 0);
+        int ones = accumulate(nums.begin(), nums.end(), 0);
+        nums.insert(nums.end(), nums.begin(), nums.end());
 
-        for(int i = 0; i < n; i++){
-            nums.push_back(nums[i]);
+        int curOnes = 0, maxOnes = 0;
+        for(int i = 0; i < ones; i++){
+            curOnes+= nums[i];
         }
-        for(int i = 1; i < nums.size(); i++){
-            nums[i] += nums[i - 1];
+        maxOnes = curOnes;
+
+        // sliding window
+        for(int i = ones; i < nums.size(); i++){
+            curOnes += nums[i] - nums[i - ones];
+            maxOnes = max(maxOnes, curOnes);
         }
-        int ones = (!len ? 0 : nums[len - 1]);
-        int ans = len - ones;
-        for(int i = len; i < nums.size(); i++){
-            int zeros = len - (nums[i] - nums[i - len]);
-            ans = min(ans, zeros);
-        }
-        return ans;
+        return ones - maxOnes;
         
     }
 };
+
