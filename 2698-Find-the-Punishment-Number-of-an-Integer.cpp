75e0f346d3fc89004ef47dd4f80af09518391cde
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-15 20:27:42 +0200

    [2698] [Time Beats: 86.65%] [Memory Beats: 91.69%] - LeetPush

---
+++ 2698-Find-the-Punishment-Number-of-an-Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool solve(int num, int target){
+        if(num == target) return true;
+        if(num < target || target < 0) return false;
+        return solve(num/10, target - num%10) || solve(num/100, target - num%100) || solve(num/1000, target - num%1000);
+    }
+    int punishmentNumber(int n) {
+        int ans = 0;
+        for(int i = 1; i<= n; i++){
+            if(solve(i*i, i)) ans+=i*i;
+        }
+        return ans;
+    }
+};
