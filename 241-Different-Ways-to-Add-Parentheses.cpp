0e915bce3ebfe3ba379fa18e1f857f689dac5481
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-16 02:12:08 +0200

    [241] [Time Beats: 78.88%] [Memory Beats: 88.27%] - LeetPush

--- 241-Different-Ways-to-Add-Parentheses.cpp
+++ 241-Different-Ways-to-Add-Parentheses.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string expression) {
+    vector<int> memo[21][21];
+    vector<int> solve(int i, int j, string& s){
+        if(i == j) return {s[i] - '0'};
+        if(i+1 == j) return {(s[i] - '0') * 10 + s[j] - '0'};
+        if(memo[i][j].size()) return memo[i][j];
         vector<int> v;
-        for(int i = 0; i < expression.size(); i++){
-            char op = expression[i];
+        for(int k = i+1; k<j; k++){
+            char op = s[k];
             if(op == '+' || op == '-' || op == '*'){
-                vector<int> left = diffWaysToCompute(expression.substr(0,i));
-                vector<int> rigth = diffWaysToCompute(expression.substr(i+1));
+                vector<int> left = solve(i, k-1, s);
+                vector<int> rigth = solve(k+1, j, s);
                 for(int l :left)
                     for(int r : rigth)
                         if(op == '+') v.push_back(l+r);
@@ -14,9 +18,9 @@ public:
                         else v.push_back(l*r);
             }
         }
-        if(v.empty())
-            v.push_back(stoi(expression));
-        
-        return v;
+        return memo[i][j] = v;
+    }
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(0, expression.size()-1, expression);
     }
 };
