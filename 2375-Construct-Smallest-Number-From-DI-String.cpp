8d599d34f52941a43b8ac94ad5339556d0f0b0cf
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-18 21:12:26 +0200

    [2375] [Time Beats: 100.00%] [Memory Beats: 63.49%] - LeetPush

--- 2375-Construct-Smallest-Number-From-DI-String.cpp
+++ 2375-Construct-Smallest-Number-From-DI-String.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    bitset<10> vis;
-    string ans;
-
-    bool solve(int idx,string& pattern){
-        if(idx == pattern.size()) return ans.size() == idx+1;
-        for(char c = '1'; c <= '9'; c++){
-            if(vis[c]) continue;
-            if(pattern[idx] == 'I' && c <= ans.back()) continue;
-            if(pattern[idx] == 'D' && c >= ans.back()) continue;
-            vis[c] = true;
-            ans.push_back(c);
-            if(solve(idx +1, pattern)) return true;
-            vis[c] = false;
-            ans.pop_back();
-
-        }
-        return false;
-    }
     string smallestNumber(string pattern) {
+        string ans, stk;
+        int idx = 0;
         for(char c = '1'; c <= '9'; c++){
-            vis[c] = true;
-            ans.push_back(c);
-            if(solve(0, pattern)) return ans;
-            vis[c] = false;
-            ans.pop_back();
-
+            stk.push_back(c);
+            if(pattern[idx] == 'I' || idx == pattern.size()){
+                while(stk.size()){
+                    ans.push_back(stk.back());
+                    stk.pop_back();
+                }
+            } 
+            idx++;
         }
-        return "-1";
+        
+        return ans;
     }
 };
