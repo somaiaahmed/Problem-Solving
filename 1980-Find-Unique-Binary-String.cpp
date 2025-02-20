8cadcb2850670fb6a6de0af023b95fe80a568682
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-20 13:37:28 +0200

    [1980] [Time Beats: 100.00%] [Memory Beats: 94.35%] - LeetPush

--- 1980-Find-Unique-Binary-String.cpp
+++ 1980-Find-Unique-Binary-String.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    string solve(string s, vector<string>& nums){
+    bool solve(string& s, vector<string>& nums){
         if(s.size() == nums.size())
-            return find(nums.begin(), nums.end(), s) == nums.end() ? s : "";
-        return max(solve(s+"0", nums), solve(s+"1", nums));
+            return find(nums.begin(), nums.end(), s) == nums.end() ? true : false;
+        s.push_back('0');
+        if(solve(s, nums)) return true;
+        s.pop_back();
+
+        s.push_back('1');
+        if(solve(s, nums)) return true;
+        s.pop_back();
+        return false;
     }
     string findDifferentBinaryString(vector<string>& nums) {
-        return solve("", nums);
+        string s;
+        solve(s, nums);
+        return s;
     }
 };
