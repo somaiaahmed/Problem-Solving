73affa24bd5f901a39c25ae260f6ed670c25c480
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-24 23:20:45 +0200

    [2762] [Time Beats: 58.00%] [Memory Beats: 74.83%] - LeetPush

--- 2762-Continuous-Subarrays.cpp
+++ 2762-Continuous-Subarrays.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        map<int, int> freq;
-        int left =0, right= 0;
-        int n = nums.size();
-        long long count = 0;
-
-        while(right <n){
-            freq[nums[right]]++;
-
-            while(freq.rbegin()->first - freq.begin()->first > 2){
-                freq[nums[left]]--;
-                if(freq[nums[left]] == 0){
-                    freq.erase(nums[left]);
-                }
-                left++;
+        long long sum = 0;
+        int l = 0, r = 0, n = nums.size();
+        map<int, int>mp; 
+        while(r < n){
+            mp[nums[r++]]++;
+            while(mp.rbegin()->first - mp.begin()->first > 2){
+                mp[nums[l]]--;
+                if(mp[nums[l]] == 0)
+                    mp.erase(nums[l]);
+                l++;
             }
-            count += right - left +1;
-            right++;
+            sum+= r - l;
         }
-        return count;
+        return sum;
         
     }
 };
