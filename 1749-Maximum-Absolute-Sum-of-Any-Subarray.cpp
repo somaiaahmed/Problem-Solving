5ea7758218e3fa2d4c621c04a98dc79c59f3e72e
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-02-26 14:52:37 +0200

    [1749] [Time Beats: 100.00%] [Memory Beats: 51.09%] - LeetPush

--- 1749-Maximum-Absolute-Sum-of-Any-Subarray.cpp
+++ 1749-Maximum-Absolute-Sum-of-Any-Subarray.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int sum = 0, mx = 0, mn = 0, ans = 0;
+        int sum = 0, mx = 0, mn = 0;
         for(auto num: nums){
             sum+=num;
 
-            ans = max(ans, sum);
-            ans = max(ans, -sum);
-
-            mx = max(mx, sum);
-            mn = min(mn, sum);
-
-            ans = max(ans, abs(sum - mx));
-            ans = max(ans, abs(sum - mn));
+            mn = min(sum, mn);
+            mx = max(sum, mx);
+            
         }
-        return ans;
+        return mx - mn;
     }
 };
