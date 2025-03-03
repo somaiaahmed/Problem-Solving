6cea33762b25619396828df41cd26d4601771eb7
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-03-03 21:37:03 +0200

    [2161] [Time Beats: 80.50%] [Memory Beats: 71.33%] - LeetPush

--- Partition-Array-According-to-Given-Pivot.cpp
+++ Partition-Array-According-to-Given-Pivot.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int>small, great;
         vector<int> ans;
         ans.reserve(n);
         int cpivot = 0;
-        for(auto x : nums){
-            if(x<pivot) small.push_back(x);
-            else if(x>pivot) great.push_back(x);
-            else cpivot++;
-        }
-        
-        ans.insert(ans.end(), small.begin(), small.end());
-        ans.insert(ans.end(), cpivot, pivot);
-        ans.insert(ans.end(), great.begin(), great.end());
+        for(auto x : nums) if(x<pivot) ans.push_back(x);
+        for(auto x : nums) if(x == pivot) ans.push_back(x);
+        for(auto x : nums) if(x>pivot) ans.push_back(x);
+           
         return ans;
     }
 };
