fe547f9f33e96f51cd02209e0bc24fc314ffc717
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-03-05 21:32:40 +0200

    [2579] [Time Beats: 100.00%] [Memory Beats: 31.17%] - LeetPush

--- Count-Total-Number-of-Colored-Cells.cpp
+++ Count-Total-Number-of-Colored-Cells.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     long long coloredCells(int n) {
         long long ans = 1, bound = 4;
-        for(int i = 2; i<=n; i++){
-            ans+=bound;
-            bound+=4;
+        if(n==1)return 1;
+        n--;
+        // for(int i = 2; i<=n; i++){
+        //     ans+=bound;
+        //     bound+=4;
 
-            // cout<<"i= "<< i << " ans = "<<ans<<endl;
-        }
-        return ans;
+        //     // cout<<"i= "<< i << " ans = "<<ans<<endl;
+        // }
+        return 1LL*2*n*(n+1)+1;
     }
 };
