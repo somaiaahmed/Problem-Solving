d6dc812f99c91ac23fba406ee9c21b69a39f285d
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-03-06 16:39:05 +0200

    [2965] [Time Beats: 100.00%] [Memory Beats: 90.26%] - LeetPush

--- Find-Missing-and-Repeated-Values.cpp
+++ Find-Missing-and-Repeated-Values.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        unordered_map<int,int> mp;
-        vector<int>ans;
+        int n = grid.size(), a, b;
+        bitset<2501> mp;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                mp[grid[i][j]]++;
+               if(mp[grid[i][j]])
+                    a = grid[i][j];
+                mp[grid[i][j]] = 1;
             }
         }
-        int a,b;
         for(int i = 1; i <= n*n; i++){
-            if(mp[i] == 2)  a = i;
             if(mp[i] == 0)  b = i;
         }
-        
-        ans.push_back(a);
-        ans.push_back(b);
-        return ans;
+
+        return {a, b};
     }
 };
