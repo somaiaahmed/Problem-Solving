ffe88f44247329315bf635dbda59cd4790bb8fb2
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-03-07 21:39:46 +0200

    [2523] [Time Beats: 79.06%] [Memory Beats: 93.21%] - LeetPush

--- Closest-Prime-Numbers-in-Range.cpp
+++ Closest-Prime-Numbers-in-Range.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    bool isPrime(int n){
-        if(n == 2 || n == 3)return true;
-        if(n % 2 == 0 || n % 3 == 0 || n <= 1)return false;
-        for(int i = 5; i*i <= n; i+= 6)
-            if(n % i == 0 || n % (i+2) == 0)return false;
-        return true;
-    } 
+    bitset<1000001> vis;
+    void seive(int n){
+        vis[0] = vis[1] = 1;
+        for(int i = 2; i*i <= n; i++){
+            if(!vis[i])
+                for(int j = i+i; j <= n; j+=i) vis[j] = 1;
+        }
+    }
     vector<int> closestPrimes(int left, int right) {
-        vector<int> prime, ans(2);
-        int mn = 1e9;
+        seive(right);
+        int mn = 1e9, a = -1, b = -1, lst = -1;
         for(int i = left; i <= right; i++){
-            if(isPrime(i)) prime.push_back(i);
-        }
-        if(prime.size() < 2) return {-1, -1};
-
-        for(int i = 0; i < prime.size() - 1; i++){
-            if(prime[i+1] - prime[i] < mn){
-                mn = prime[i+1] - prime[i];
-                ans = {prime[i], prime[i+1]}; 
+            if(vis[i]) continue;
+            if(lst != -1 && i - lst < mn){
+                mn = i - lst;
+                a = lst, b = i;
             }
+            lst = i;
         }
-        return ans;
+       return {a, b};
     }
 };
