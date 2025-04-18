8f19f536ea7307bed133e811be34d68d26c0a04b
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2025-04-18 14:54:34 +0200

    [38] [Time Beats: 100.00%] [Memory Beats: 97.27%] - LeetPush

---
+++ Count-and-Say.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return \1\;
+        string ret = countAndSay(n-1);
+        string ans = \\;
+        int cnt = 1;
+        for(int i = 0; i < ret.size(); i++){
+            if(ret[i] == ret[i+1])cnt++;
+            else{
+                ans.push_back(cnt+'0');
+                ans.push_back(ret[i]);
+                cnt=1;
+            }
+        }
+        return ans;
+        
+    }
+};
